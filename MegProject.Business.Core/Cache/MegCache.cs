1a0fd0ede8ff256ccbe2d8d950e45fda5d9cf766
MURATGUVEN <MURATGUVEN@DESKTOP-K4S9J46>
2016-07-09 15:57:10 +0300

    Continue Caching Design

---
+++ MegProject.Business.Core/Cache/MegCache.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MegProject.Business.Core.Cache
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public sealed class MegCache : Attribute
+    {
+       
+        private readonly int _duration;
+
+        // This is a positional argument
+        public MegCache(int duration)
+        {
+            this._duration = duration;            
+        }
+
+        public int Duration
+        {
+            get { return this._duration; }             
+        }
+
+        
+    }
+  
+}
